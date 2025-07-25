c111bf25fb3825393a7ca77398fc9fe1a073fcf9
Alex Forsythe <alexwforsythe@gmail.com>
2025-07-24 20:59:14 -0700

    configs: add more docker aliases

--- configs/aliases.sh
+++ configs/aliases.sh
@@ -63,3 +63,9 @@ alias gcpc="git cherry-pick --continue"
 alias gcop="git checkout '@{-1}'"
 alias gwip="git add -A && git commit -m 'wip'"
 alias grim="git rebase --interactive \$(git merge-base HEAD origin/master)"
+
+# docker (extends prezto/modules/docker)
+alias dkB="docker builder"
+alias dkBpr="docker builder prune"
+alias dkBprA="docker builder prune --all"
+alias dkprA="docker container prune && docker image prune && docker volume prune"
