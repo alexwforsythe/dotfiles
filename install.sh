999ad65184aea85d02a1ffe53e74f76b8213df0a
Alex Forsythe <alexwforsythe@gmail.com>
2025-07-28 13:40:17 -0700

    feat: manage zsh and prezto configs with chezmoi

--- install.sh
+++ install.sh
@@ -31,11 +31,3 @@ git -C "$dotbot_dir" submodule update --init --recursive
   --plugin-dir dotbot-brew \
   --config-file "$cfg_file" \
   "$@"
-
-setopt EXTENDED_GLOB
-dotfiles_dir=${XDG_DATA_HOME:-$HOME/.local/share}/dotfiles
-# shellcheck disable=1036,1058,1072,1073
-for rcfile in "$dotfiles_dir"/configs/^README.md(.N); do
-  ln -s "$rcfile" "$dotfiles_dir/configs/.${rcfile:t}"
-done
-unset dotfiles_dir
