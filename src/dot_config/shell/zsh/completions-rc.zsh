9d6024602e694e318712b3ef0a1ebd3ce0b122e5
Alex Forsythe <alexwforsythe@gmail.com>
2025-08-18 14:38:54 -0700

    feat(zsh): update completion tag formats

--- src/dot_config/shell/zsh/completions-rc.zsh
+++ src/dot_config/shell/zsh/completions-rc.zsh
@@ -36,109 +36,110 @@ setopt menucomplete
 #
 # Formats
 #
-# 󰅩 󰅪  󰅴 󰅲
-#
+
+zstyle ':completion:*' list-separator 
 
 _tagfmt () {
-  # shellcheck disable=2299
-  echo "%F{${2:-8}%B${1:-󰌕} ${4:-%d}${${3:+ $3}:-}%b%f"
+  # @note We use normal printf escape sequences to avoid the "do you wish to see
+  # all possibilities" prompt:
+  # https://github.com/marlonrichert/zsh-autocomplete/issues/654
+  zstyle ":completion:*${1:+:$1}" \
+    format $(pprint ${3:-8} bold "${2:-󰌕} ${5:-%d}${${4:+ $4}:-}")
+    # format "%F{${3:-8}%B${2:-󰌕} ${5:-%d}${${4:+ $4}:-}%b%f"
 }
 
-# Remove conflicting settings from zsh-autocomplete.
-zstyle -d ':completion:*:-command-:*' group-name
-zstyle -d ':completion:*:-command-:*' format
-# zstyle -d ':completion:*:-command-:*' file-patterns
-# zstyle -d ':completion:*:-command-:*' tag-order
-# zstyle -d ':completion:*:all-expansions' group-name
-
 # default
-zstyle ':completion:*:heads' format "$(_tagfmt )" # 󱉟
-zstyle ':completion:*:aliases' format "$(_tagfmt )" #   
-zstyle ':completion:*:all-expansions' format "$(_tagfmt )"
-zstyle ':completion:*:all-files' format "$(_tagfmt )"
-zstyle ':completion:*:argument-rest' format "$(_tagfmt )"
-zstyle ':completion:*:arguments' format "$(_tagfmt 󰫧)" #  󰫧 󰺲
-zstyle ':completion:*:arrays' format "$(_tagfmt 󰅪)"
-zstyle ':completion:*:association-keys' format "$(_tagfmt )"
-zstyle ':completion:*:builtins' format "$(_tagfmt 󱆃)"
-zstyle ':completion:*:colors' format "$(_tagfmt )" # 
-zstyle ':completion:*:commands' format "$(_tagfmt )" #  󰏓
-zstyle ':completion:*:corrections' format "$(_tagfmt  green '(errors: %e)')" # 󰅏  
-zstyle ':completion:*:descriptions' format "$(_tagfmt )"
-zstyle ':completion:*:directories' format "$(_tagfmt )"
-zstyle ':completion:*:directory-stack' format "$(_tagfmt )" # 
-zstyle ':completion:*:domains' format "$(_tagfmt 󰇗)" #   󰇖
-zstyle ':completion:*:events' format "$(_tagfmt )"
-zstyle ':completion:*:expansions' format "$(_tagfmt 󰑑)" # 
-zstyle ':completion:*:files' format "$(_tagfmt )"
-zstyle ':completion:*:fonts' format "$(_tagfmt )" # 
-zstyle ':completion:*:functions' format "$(_tagfmt 󰊕)"
-zstyle ':completion:*:globbed-files' format "$(_tagfmt )" # 󰙅  
-zstyle ':completion:*:groups' format "$(_tagfmt )"
-zstyle ':completion:*:history-words' format "$(_tagfmt )"
-zstyle ':completion:*:history-lines' format "$(_tagfmt )" # 󰯓 󰯂
-zstyle ':completion:*:hosts' format "$(_tagfmt )"
-zstyle ':completion:*:jobs' format "$(_tagfmt )" #  
-zstyle ':completion:*:keymaps' format "$(_tagfmt )" # 󰌓 󰌌
-zstyle ':completion:*:libraries' format "$(_tagfmt )" # 󱉟
-zstyle ':completion:*:ls:*:argument-rest' format "$(_tagfmt )"
-zstyle ':completion:*:manuals.*' format "$(_tagfmt )" #  
-zstyle ':completion:*:messages' format "$(_tagfmt 󰍢 purple)" #  
-zstyle ':completion:*:modules' format "$(_tagfmt )" #  
-zstyle ':completion:*:named-directories' format "$(_tagfmt 󰲂)"
-zstyle ':completion:*:names' format "$(_tagfmt 󰌕)"
-zstyle ':completion:*:options' format "$(_tagfmt )"
-zstyle ':completion:*:original' format "$(_tagfmt 󰒊)"
-zstyle ':completion:*:parameters' format "$(_tagfmt )" # 󰫧 󱃗
-zstyle ':completion:*:path-dirs' format "$(_tagfmt 󱞊)" # 󰴉
-zstyle ':completion:*:ports' format "$(_tagfmt 󱇢)" # 󱂇
-zstyle ':completion:*:processes' format "$(_tagfmt )"
-zstyle ':completion:*:processes-names' format "$(_tagfmt )"
-zstyle ':completion:*:recent-dirs' format "$(_tagfmt )" # 󰪺 󰉓
-zstyle ':completion:*:reserved-words' format "$(_tagfmt )"
-zstyle ':completion:*:signals' format "$(_tagfmt )" # 
-zstyle ':completion:*:strings' format "$(_tagfmt )" # 
-zstyle ':completion:*:styles' format "$(_tagfmt )" # zstyles
-zstyle ':completion:*:suffixes' format "$(_tagfmt )" # 󰨿 󰈤 file extensions
-zstyle ':completion:*:suffix-aliases' format "$(_tagfmt 󰌧)" #   󰘧
-zstyle ':completion:*:targets' format "$(_tagfmt )" # makefile targets
-zstyle ':completion:*:users' format "$(_tagfmt )"
-zstyle ':completion:*:values' format "$(_tagfmt )" # 
-zstyle ':completion:*:warnings' format "$(_tagfmt  red '' 'no matches found')"
-zstyle ':completion:*:widgets' format "$(_tagfmt 󰜬)" # 
-zstyle ':completion:*:zsh-options' format "$(_tagfmt )" #   󱕂
-zstyle ':completion:*' format "$(_tagfmt)"
+# Set the default completion format.
+_tagfmt
+# Override the default -command- format set by zsh-autocomplete.
+_tagfmt '-command-:*'
+_tagfmt heads  # 󱉟
+_tagfmt aliases  #   
+_tagfmt all-expansions 
+_tagfmt all-files 
+_tagfmt argument-rest 
+_tagfmt arguments 󰫧 #  󰫧 󰺲
+_tagfmt arrays 󰅪
+_tagfmt association-keys 
+_tagfmt builtins 󱆃
+_tagfmt colors  # 
+_tagfmt commands  #  󰏓
+_tagfmt corrections  green '(errors: %e)' # 󰅏  
+_tagfmt descriptions 
+_tagfmt directories 
+_tagfmt directory-stack  # 
+_tagfmt domains 󰇗 #   󰇖
+_tagfmt events 
+_tagfmt expansions 󰑑 # 
+_tagfmt files 
+_tagfmt fonts  # 
+_tagfmt functions 󰊕
+_tagfmt globbed-files  # 󰙅  
+_tagfmt groups 
+_tagfmt history-words 
+_tagfmt history-lines  # 󰯓 󰯂
+_tagfmt hosts 
+_tagfmt jobs  #  
+_tagfmt keymaps  # 󰌓 󰌌
+_tagfmt libraries  # 󱉟
+_tagfmt 'ls:*:argument-rest' 
+_tagfmt 'manuals.*'  #  
+_tagfmt messages 󰍢 purple #  
+_tagfmt modules  #  
+_tagfmt named-directories 󰲂
+_tagfmt names 󰌕
+_tagfmt options 
+_tagfmt original 󰒊
+_tagfmt parameters  # 󰫧 󱃗
+_tagfmt path-dirs 󱞊 # 󰴉
+_tagfmt ports 󱇢 # 󱂇
+_tagfmt processes 
+_tagfmt processes-names 
+_tagfmt recent-dirs  # 󰪺 󰉓
+_tagfmt reserved-words 
+_tagfmt signals  # 
+_tagfmt strings  # 
+_tagfmt styles  # zstyles
+_tagfmt suffixes  # 󰨿 󰈤 file extensions
+_tagfmt suffix-aliases 󰌧 #   󰘧
+_tagfmt targets  # makefile targets
+_tagfmt users 
+_tagfmt values  # 
+_tagfmt warnings  red '' 'no matches found'
+_tagfmt widgets 󰜬 # 
+_tagfmt zsh-options  #   󱕂
 
 # git
-zstyle ':completion:*:blob-objects' format "$(_tagfmt )"
-zstyle ':completion:*:blob-tags' format "$(_tagfmt )"
-zstyle ':completion:*:blobs-and-trees-in-treeish' format "$(_tagfmt )" # 
-zstyle ':completion:*:blobs' format "$(_tagfmt )"
-zstyle ':completion:*:branch-names' format "$(_tagfmt )"
-zstyle ':completion:*:cached-files' format "$(_tagfmt )" # 
-zstyle ':completion:*:changed-in-index-files' format "$(_tagfmt )" # 
-zstyle ':completion:*:changed-in-working-tree-files' format "$(_tagfmt )"
-zstyle ':completion:*:commit-objects' format "$(_tagfmt )"
-zstyle ':completion:*:commit-ranges' format "$(_tagfmt )"
-zstyle ':completion:*:commit-tags' format "$(_tagfmt )"
-zstyle ':completion:*:commits' format "$(_tagfmt )" # 
-zstyle ':completion:*:heads-local' format "$(_tagfmt )" # 
-zstyle ':completion:*:heads-remote' format "$(_tagfmt )" # 
-zstyle ':completion:*:heads' format "$(_tagfmt )"
-zstyle ':completion:*:local-repositories' format "$(_tagfmt )"
-zstyle ':completion:*:modified-files' format "$(_tagfmt )"
-zstyle ':completion:*:other-files' format "$(_tagfmt )"
-zstyle ':completion:*:recent-branches' format "$(_tagfmt )"
-zstyle ':completion:*:references' format "$(_tagfmt )" # 
-zstyle ':completion:*:remote-branch-names-noprefix' format "$(_tagfmt )"
-zstyle ':completion:*:remote-repositories' format "$(_tagfmt )" #  
-zstyle ':completion:*:remotes' format "$(_tagfmt )"
-zstyle ':completion:*:revisions' format "$(_tagfmt )"
-zstyle ':completion:*:stashes' format "$(_tagfmt )"
-zstyle ':completion:*:submodules' format "$(_tagfmt )"
-zstyle ':completion:*:tree-ish-to-index-files' format "$(_tagfmt )"
-zstyle ':completion:*:tree-ishs' format "$(_tagfmt )" # 
-zstyle ':completion:*:untracked-files' format "$(_tagfmt )" # 
+# @todo set group-order zstyle for specific commands
+_tagfmt blob-objects 
+_tagfmt blob-tags 
+_tagfmt blobs-and-trees-in-treeish  # 
+_tagfmt blobs 
+_tagfmt branch-names 
+_tagfmt cached-files  # 
+_tagfmt changed-in-index-files  # 
+_tagfmt changed-in-working-tree-files  # 
+_tagfmt commit-objects 
+_tagfmt commit-ranges 
+_tagfmt commit-tags 
+_tagfmt commits  # 
+_tagfmt heads-local  # 
+_tagfmt heads-remote  # 
+_tagfmt heads 
+_tagfmt local-repositories 
+_tagfmt modified-files 
+_tagfmt other-files 
+_tagfmt recent-branches 
+_tagfmt references  # 
+_tagfmt remote-branch-names-noprefix 
+_tagfmt remote-repositories  #  
+_tagfmt remotes 
+_tagfmt revisions 
+_tagfmt stashes 
+_tagfmt submodules 
+_tagfmt tree-ish-to-index-files 
+_tagfmt tree-ishs  # 
+_tagfmt untracked-files  # 
 
 # docker ( containers,  images)
 # @todo
