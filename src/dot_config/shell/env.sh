6902639d8b5295811abd2f22044f6e18c070c887
Alex Forsythe <alexwforsythe@gmail.com>
2026-05-19 15:58:45 -0700

    chore(shell): move zsh-specific env vars from env.sh to .zshenv

--- src/dot_config/shell/env.sh
+++ src/dot_config/shell/env.sh
@@ -15,6 +15,3 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CACHE_HOME="$HOME/.cache"
-
-export ZDOTDIR="$HOME"
-export ZPLUGINDIR="$XDG_DATA_HOME/zsh-plugins"
